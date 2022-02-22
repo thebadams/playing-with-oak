cde2efa2e1783a57eddc4c38dae6cb5bf31ec207
TheBadams <bpeteradams@gmail.com>
2022-02-21 23:15:27 -0500

    Mongodb(Deps): added Bson, Mongo Client to dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,8 @@
-import { Application, Context, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Context, Router } from "https://deno.land/x/oak@v10.2.0/mod.ts";
 
-export { Application, Context, Router };
+import {
+	Bson,
+	MongoClient,
+} from "https://deno.land/x/mongo@v0.29.2/mod.ts";
+
+export { Application, Context, Router, Bson, MongoClient };
