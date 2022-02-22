6b4c8a6a13dd558e034e0d218e97d93f91a7aa06
TheBadams <bpeteradams@gmail.com>
2022-02-22 10:01:30 -0500

    Formatting: ran deno fmt

--- server/routes/person.router.ts
+++ server/routes/person.router.ts
@@ -1,8 +1,8 @@
-import { Router } from '../../deps.ts';
-import {createPerson} from '../controllers/createPerson.controller.ts'
+import { Router } from "../../deps.ts";
+import { createPerson } from "../controllers/createPerson.controller.ts";
 const personRouter = new Router();
 
 personRouter
-	.post('/person', createPerson);
+  .post("/person", createPerson);
 
-export {personRouter};
+export { personRouter };
