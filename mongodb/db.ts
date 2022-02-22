9252f4643fe84585dd2025b1490ee634d38aa5c3
TheBadams <bpeteradams@gmail.com>
2022-02-22 09:34:47 -0500

    Mongodb(Bugfix): Changed connection uri to 127.0.0.1 vs localhost; fixed problem, no longer crashes

--- mongodb/db.ts
+++ mongodb/db.ts
@@ -2,7 +2,7 @@ import { MongoClient } from "../deps.ts";
 
 const client = new MongoClient();
 
-await client.connect('mongodb://localhost:27017');
+await client.connect('mongodb://127.0.0.1:27017');
 
 const db = client.database('deno');
 
